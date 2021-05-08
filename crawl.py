c716104169d425bdac8a6925b4a80073d43e96a0
snakeeye98 <56976889+snakeeye98@users.noreply.github.com>
2021-05-08 16:11:35 +0200

    improved & pretty json

--- crawl.py
+++ crawl.py
@@ -4,12 +4,14 @@ import os
 from bs4 import BeautifulSoup
 
 n_threads = 5   #amount of threads used for crawling
+url_toCrawl = "reddit.com/r/MachineLearning/*"  #URL to crawl
+indexes_toCrawl = ["2019-47"]  #indexes to crawl from CommonCrawl
 
-client = IndexClient(["2019-47"], verbose=True) #initialize client to crawl
+client = IndexClient(indexes_toCrawl, verbose=False) #initialize client to crawl, change verbose to see logs
 
 if not os.path.isfile('results.csv'):
-    
-    client.search("reddit.com/r/MachineLearning/*", threads=n_threads)  #specifiy crawling attributes (you may enter a keyword)
+    print("Crawling all data for the URL {} and the indexes {}...".format(url_toCrawl, indexes_toCrawl))
+    client.search(url_toCrawl, threads=n_threads)  #specifiy crawling attributes (you may enter a keyword)
     client.results = (pd.DataFrame(client.results)      #create a pandas dataframe which is sorted by timestamp. duplicates are excluded
                     .sort_values(by="timestamp")
                     .drop_duplicates("urlkey", keep="last")
@@ -17,22 +19,26 @@ if not os.path.isfile('results.csv'):
     client.download(threads=n_threads)  #start crawling
 
     pd.DataFrame(client.results).to_csv("results.csv")  #save the crawled dataframe to a csv file
-
+else:
+    print("There already exists a results.csv file. Skipping crawling.")
 df = pd.read_csv("results.csv") #read out the dataframe from the csv file
 
 html_body = df['html']
-results = html_body #copy the dataframe to create one of the same size, values will get overwritten
-count = 0
 
+count = 0
+list_df = []    #the list which will be used to create a dataframe later
+
+print("Handling the crawled data...")
 for element in html_body:   #use beautiful soup to extract useful text from the crawled html formatted string 
     soup = BeautifulSoup(element, 'html.parser')
-    results[count] = soup.title.get_text() + " --- " + soup.body.get_text()
+    list_df.append([soup.title.get_text(), soup.body.get_text()])   #append title and text to the list
     count += 1    
 
-print(results)
-print('Amount of crawled articles: {}\n'.format(count))
+results = pd.DataFrame(list_df, columns=["title", "body"])  #create a dataframe from the list
+print('Amount of crawled articles: {}'.format(count))
 
-results.to_json('results.json') #save the formatted texts (html excluded) to a json-layout 
+results.to_json('results.json', orient='index', indent=2) #save the formatted texts (html excluded) to a json-layout 
+print("All crawled data has been written to results.json.")
 
 
 
