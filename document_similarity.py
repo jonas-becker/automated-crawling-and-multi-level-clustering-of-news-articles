27fdea35a8429e292c0c943778e0a71aaf176cc5
snakeeye98 <snakeeye98@web.de>
2021-06-02 22:00:46 +0200

    tf idf

--- document_similarity.py
+++ document_similarity.py
@@ -1,6 +1,8 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.feature_extraction.text import TfidfVectorizer
+import math
 import pandas as pd
 
 PATH = '.\crawl_json\crawl_0_0.json'
@@ -22,20 +24,30 @@ def get_features(text_list):
     print('\n')
     return features
 
+def tf_idf_vectorizer(corpus):
+    tfIdfVectorizer=TfidfVectorizer(use_idf=True)
+    tfIdf = tfIdfVectorizer.fit_transform(corpus)
+    df = pd.DataFrame(tfIdf[0].T.todense(), index=tfIdfVectorizer.get_feature_names(), columns=["TF-IDF"])
+    df = df.sort_values('TF-IDF', ascending=False)
+    print (df.head(25))
+    return df
+
 def main():
     df = load_data_from_csv()
     title_list = get_text_list(df, "title")
     maintext_list = get_text_list(df, "maintext")
-    features_titles = get_features(title_list)
+    #features_titles = get_features(title_list)
     features_maintext = get_features(maintext_list)
-    print("_______________TITLE COMPARISON _____________\n")
-    for i, f in enumerate(features_titles):
-        print(f'EUCLIDIAN - Index: {i} distance: {euclidean_distances(features_titles[2], f)}')
+    #print("_______________TITLE COMPARISON _____________\n")
+    #for i, f in enumerate(features_titles):
+        #print(f'EUCLIDIAN - Index: {i} distance: {euclidean_distances(features_titles[2], f)}')
 
     print("_______________MAINTEXT COMPARISON _____________\n")
     for i, f in enumerate(features_maintext):
         print(f'COSINE - Index: {i} distance: {cosine_similarity(features_maintext[2], f)}')
 
+    print(maintext_list)
+    df = tf_idf_vectorizer(maintext_list)   #needs to be changed
 
 if __name__ == '__main__':
     main()
