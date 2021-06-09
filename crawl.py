599fa7df7660fd6a338c8108d72573be40754006
Nicolas Kossmann <n.22koss@gmail.com>
2021-06-09 11:09:27 +0200

    fixed

--- crawl.py
+++ crawl.py
@@ -18,7 +18,7 @@ from botocore.handlers import disable_signing
 from langdetect import detect
 
 TARGET_WEBSITES = [".cnn.com", ".washingtonpost.com", ".nytimes.com", ".abcnews.go.com", ".bbc.com", ".cbsnews.com", ".chicagotribune.com", ".foxnews.com", ".huffpost.com", ".latimes.com", ".nbcnews.com", ".npr.org/sections/news", ".politico.com", ".reuters.com", ".slate.com", ".theguardian.com", ".wsj.com", ".usatoday.com", ".breitbart.com", ".nypost.com", ".cbslocal.com", ".nydailynews.com", ".newsweek.com", ".boston.com", ".denverpost.com", ".seattletimes.com", ".miamiherald.com", ".observer.com", ".washingtontimes.com", ".newsday.com", ".theintercept.com"]  #these trings will be compared with the URL and if matched added to datasets. You may add a specific path you are looking for
-TEST_TARGETS = ["nytimes.com", ".washingtonpost.com"]
+TEST_TARGETS = [".nytimes.com", ".washingtonpost.com"]
 INDEXES = ['2020-16', "2021-21"]    #The indexes from commoncrawl
 MAX_ARCHIVE_FILES_PER_URL = 2   #Change to increase or decrease the amount of crawled data per URL (Estimated size per archive: 1.2 GB)
 MINIMUM_MAINTEXT_LENGTH = 200
@@ -202,7 +202,7 @@ def download_archives(warc_paths, all_index):
             nonlocal downloaded
             downloaded += chunk
             done = int(50 * downloaded / total_length)
-            sys.stdout.write("\r[%s%s] %s MB / %s MB" % ('=' * done, ' ' * (50-done), str(round(downloaded/100000)), str(round(total_length/100000))))
+            sys.stdout.write("\r[%s%s] %s MB / %s MB\n" % ('=' * done, ' ' * (50-done), str(round(downloaded/100000)), str(round(total_length/100000))))
             sys.stdout.flush()
 
         if os.path.isfile(f"crawl_data/crawled_data_{str(all_index)}_{str(index)}.warc.gz") is False: 
